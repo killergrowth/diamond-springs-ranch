1fbd2e21bd19682d5aee4b8fd10ea1104654e187
killergrowth <notifications@killergrowth.com>
2026-09-22 10:33:04 -0500

    Replace about page family photo with DSC09289.jpeg

--- build.js
+++ build.js
@@ -1101,7 +1101,7 @@ function buildAboutPage() {
   </div>
 </section>
 <section class="dsr-section-alt" style="padding:0;">
-  <img src="/images/ChatGPT_Image_Sep_4__2026__08_32_39_AM.png" alt="The Schrag family at Diamond Springs Ranch" style="width:100%;display:block;max-height:600px;object-fit:cover;object-position:center 30%;">
+  <img src="/images/DSC09289.jpeg" alt="The Schrag family at Diamond Springs Ranch" style="width:100%;display:block;max-height:600px;object-fit:cover;object-position:center 30%;">
 </section>
 ${buildV2Reviews(0)}
 ${CTA}`;
